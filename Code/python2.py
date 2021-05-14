a0e558a2bb41a64adf6eb194510a2b8692ca8ead
AmirAR-QA <aabdur-rahman@academytrainee.com>
2021-05-14 10:31:35 +0100

    finished problem 2

--- Code/python2.py
+++ Code/python2.py
@@ -51,6 +51,13 @@ def one(input):
 	# Use your CLI to access the Python documentation and get help manipulating strings - help(range).
 
 def two(input):
+	if input > 1
+		return False
+	if input = 1
+		return True
+	for i in range(2, input):
+		if input % i == 0:
+    	return False
     return False
 
 	# <QUESTION 3>
